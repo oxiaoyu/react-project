d2476a3614894e5a1407aae60cae9898d1c97dc4
oxiaoyu <825663811@qq.com>
2019-06-22 11:07:06 +0800

    animation

--- src/layout/App.js
+++ src/layout/App.js
@@ -5,17 +5,19 @@ import Find from '@/views/find';
 import User from '@/views/user';
 import Footer from '@/components/Footer'
 import { Switch, Route, Redirect }  from 'react-router-dom';
+import AnimatedRouter from 'react-animated-router'; //我们的AnimatedRouter组件
+import 'react-animated-router/animate.css'; //引入默认的动画样式定义
 
 
 const App = () => (
   <div className = "container">
-    <Switch>
+    <AnimatedRouter>
       <Route path = '/home' component = { Home } />
       <Route path = '/liCai' component = { LiCai } />
       <Route path = '/find' component = { Find } />
       <Route path = '/user' component = { User } />
       <Redirect to = '/home'  />
-    </Switch>
+    </AnimatedRouter>
     <Footer/>
   </div>
 )
