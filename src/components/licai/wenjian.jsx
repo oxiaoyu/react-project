590885b832ad5bf77e6a5e1e464e0b1529a76568
alonesmall <975026871@qq.com>
2019-06-24 22:32:06 +0800

    理财布局

---
+++ src/components/licai/wenjian.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Com = () => {
+  return (
+    <div className = "wenjian-box margin">
+      <h3>稳健理财
+        <span>
+          更多 &gt;
+        </span>
+      </h3>
+    </div>
+  )
+}
+export default Com;
--- src/views/liCai/UI.js
+++ src/views/liCai/UI.js
@@ -1,11 +1,48 @@
 import React from 'react';
-
+import '@/style/licai.scss';
+import Tuijian from '@/components/licai/tuijian.jsx';
+import Wenjian from '@/components/licai/wenjian.jsx';
 class Com extends React.Component {
   render () {
     return (
       <div className = "box">
-        <header className = "header">理财头部</header>
-        <div className = "content">理财内容</div>
+        <header className = "header ">
+          <div className = "margin">
+          <span className = "iconfont icon-zuojiantou"></span>
+            理财
+          </div>
+        </header>
+        <div className = "content">
+          <img src = ""/>
+          <div className = "moneykind margin">
+            <ul>
+              <li>
+                <span></span>
+                <p>基金</p>
+              </li>
+              <li>
+                <span></span>
+                <p>债券</p>
+              </li>
+              <li>
+                <span></span>
+                <p>保险</p>
+              </li>
+              <li>
+                <span></span>
+                <p>借贷</p>
+              </li>
+              <li className = "last">
+                <span></span>
+                <p>产品咨询</p>
+              </li>
+            </ul>
+          </div>
+
+          <Tuijian />
+
+          <Wenjian />
+        </div>
       </div>
     )
   }
