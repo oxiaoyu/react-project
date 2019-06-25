445d2fb585b8c2f24c4267e77160f590da820f48
Fabalus <875998568qq.com>
2019-06-25 15:35:23 +0800

    完成私人定制静态页面

--- src/views/customized/UI.js
+++ src/views/customized/UI.js
@@ -1,33 +1,69 @@
 import React from 'react';
 import '@/style/home/customized.scss';
 import CommonJs from '@/assets/js/common';
+import activity from '@/assets/images/activity.png';
 
 class Com extends React.Component {
-  render () {
+  render() {
     return (
-      <div className = "box-cust">
-        <header className = "header-cust">
-          <div className = 'back' onClick = { CommonJs.goBack.bind(this) }>
-            <img src = { require('@/assets/images/back.png') } alt = ''/>
+      <div className="box-cust">
+        <header className="header-cust">
+          <div className='back' onClick={CommonJs.goBack.bind(this)}>
+            <img src={require('@/assets/images/back.png')} alt='' />
           </div>
           <h3>私人定制</h3>
         </header>
-        <div className = "content">
+        <div className="content">
           <div className='main'>
-              <div className='main-t'>
-                <div className='main-t-l'>
-                  <h3>私人定制</h3>
-                  <p>根据你的需求，推荐最合适您的产品及服务</p>
-                </div>
-                <div className='main-t-r'>
-                  <img src={require('@/assets/images/go.png')} alt='' />
-                </div>
+            <div className='main-t'>
+              <div className='main-t-t'>
+                <h3>私人定制</h3>
+                <select>
+                  <option>按热门排序</option>
+                  <option>按最新排序</option>
+                </select>
               </div>
-              <div className='main-b'>
-                <img src={require('@/assets/images/activity.png')} alt='' />
+              <div className='main-t-b'>
+                <p>根据你的需求，推荐最合适您的产品及服务</p>
               </div>
             </div>
-        </div>
+          </div>
+          <div className='nav' style = {{
+            background: 'url(' + activity + ') center center / cover no-repeat'
+          }}>
+            <h3>选基金</h3>
+            <p>震荡不愁，我帮你分忧</p>
+            <span>40065人定制过</span>
+          </div>
+          <div className='nav' style = {{
+            background: 'url(' + activity + ') center center / cover no-repeat'
+          }}>
+            <h3>选基金</h3>
+            <p>震荡不愁，我帮你分忧</p>
+            <span>40065人定制过</span>
+          </div>
+          <div className='nav' style = {{
+            background: 'url(' + activity + ') center center / cover no-repeat'
+          }}>
+            <h3>选基金</h3>
+            <p>震荡不愁，我帮你分忧</p>
+            <span>40065人定制过</span>
+          </div>
+          <div className='nav' style = {{
+            background: 'url(' + activity + ') center center / cover no-repeat'
+          }}>
+            <h3>选基金</h3>
+            <p>震荡不愁，我帮你分忧</p>
+            <span>40065人定制过</span>
+          </div>
+          <div className='nav' style = {{
+            background: 'url(' + activity + ') center center / cover no-repeat'
+          }}>
+            <h3>选基金</h3>
+            <p>震荡不愁，我帮你分忧</p>
+            <span>40065人定制过</span>
+          </div>
+         </div>
       </div>
     )
   }
